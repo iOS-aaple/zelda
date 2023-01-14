f230d7ab8ef6f6c0b5d7588e6b63bef590bf7f6a
aamer Essa <aamer.es12@gmail.com>
2023-01-14 20:34:09 +0300

    V1.0

---
+++ zelda/View/FrindsView/ChatsView.swift
@@ -0,0 +1,20 @@
+//
+//  ChatsView.swift
+//  zelda
+//
+//  Created by Aamer Essa on 14/01/2023.
+//
+
+import SwiftUI
+
+struct ChatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChatsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChatsView()
+    }
+}
---
+++ zelda/View/FrindsView/FrindsView.swift
@@ -0,0 +1,20 @@
+//
+//  FrindsView.swift
+//  zelda
+//
+//  Created by Aamer Essa on 14/01/2023.
+//
+
+import SwiftUI
+
+struct FrindsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrindsView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrindsView()
+    }
+}

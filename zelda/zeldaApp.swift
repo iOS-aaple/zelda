5f5acb405b74f3f32e1b181cb9668e47acd34bd8
aamer Essa <aamer.es12@gmail.com>
2023-01-14 16:27:59 +0300

    Initial Commit

---
+++ zelda/zeldaApp.swift
@@ -0,0 +1,17 @@
+//
+//  zeldaApp.swift
+//  zelda
+//
+//  Created by Aamer Essa on 14/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct zeldaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

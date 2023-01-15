955659c91210d86922422e8b6e95a664a854f983
Ha <hassna.ahmad98@gmail.com>
2023-01-15 11:46:25 +0300

    V1.2 -> create Account View

---
+++ zelda/View/FrindsView/AccountView.swift
@@ -0,0 +1,20 @@
+//
+//  AccountView.swift
+//  zelda
+//
+//  Created by H . on 22/06/1444 AH.
+//
+
+import SwiftUI
+
+struct AccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AccountView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountView()
+    }
+}

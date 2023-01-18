1a806f754fba1f0b530e207b48839405c8b2d6e2
safcsp <mnorybh@gmail.com>
2023-01-19 00:00:27 +0300

    V 7.1 - some changes in home

--- zelda/View/Home/HomeView.swift
+++ zelda/View/Home/HomeView.swift
@@ -73,13 +73,12 @@ struct Home: View {
                         // for side bar and the details
                         ScrollView(.vertical, showsIndicators: false){
                             VStack(spacing: 20){
-                                ForEach(games) { i in
-                                    //                                       GamesCards(gamesData: i)
-                                }
                                 XO()
                                 ThePuzzle()
                                 Snake()
                                 UniverseMemory()
+                                Ludo()
+                                chess()
                                     .padding(.bottom, -50)
                             }
                         }
@@ -118,7 +117,6 @@ struct Home: View {
                         .shadow(color: Color.black.opacity(0.9), radius: 8, x: 2, y: 6)
                         .frame(maxHeight: .infinity, alignment: .bottom)
                     }
-                    //                       .edgesIgnoringSafeArea(.bottom)
                 }
             }
         }
@@ -126,213 +124,6 @@ struct Home: View {
     }
 }
 
-struct XO : View {
-    var body: some View {
-        HStack {
-            HStack{
-                Image("XO")
-                    .resizable()
-                    .frame(width: UIScreen.main.bounds.width / 4)
-                    .frame(width: 50, height: 100)
-                Spacer()
-            } .padding(.leading, 20)
-            
-            Text("Tic Tac Toe")
-                .multilineTextAlignment(.center)
-                .font(.system(size: 25, design: .monospaced))
-                .frame(maxWidth: 200)
-                .padding(.horizontal)
-                .shadow(color: Color.black.opacity(0.9), radius: 8, x: 2, y: 6)
-                .padding(.leading, -26)
-            VStack(spacing: 20){
-                Spacer(minLength: 0)
-                HStack{
-                    NavigationLink(destination:  Moves()) {
-                        Text("Play")
-                            .font(.caption)
-                            .foregroundColor(.white)
-                            .padding(.vertical, 12)
-                            .padding(.horizontal, 25)
-                            .background(Capsule().stroke(Color.white, lineWidth: 2))
-                    }
-                    .offset(x: -20, y: -50)
-                }
-                
-               
-            }
-        }
-        .frame(height: 200)
-        .background(
-            Color.white.opacity(0.2)
-                .cornerRadius(25)
-            
-                .rotation3DEffect(.init(degrees: 10), axis : (x:0, y: -1, z: 0))
-            // trim view
-                .padding(.vertical, 50)
-                .padding(.trailing, 25)
-            )
-        .padding(.horizontal)
-        .padding(.bottom, -90)
-    }
-}
-struct ThePuzzle : View {
-    var body: some View {
-        HStack {
-            HStack{
-                Image("puzzle")
-                    .resizable()
-                    .frame(width: UIScreen.main.bounds.width / 4)
-                    .frame(width: 60, height: 100)
-                Spacer()
-            } .padding(.leading, 20)
-            
-            Text("Puzzle")
-                .multilineTextAlignment(.center)
-                .font(.system(size: 25, design: .monospaced))
-                .frame(maxWidth: 200)
-                .padding(.horizontal)
-                .shadow(color: Color.black.opacity(0.9), radius: 8, x: 2, y: 6)
-                .padding(.leading, -26)
-            VStack(spacing: 20){
-                Spacer(minLength: 0)
-                HStack{
-                    NavigationLink(destination: PuzzleView()) {
-                        Text("Play")
-                            .font(.caption)
-                            .foregroundColor(.white)
-                            .padding(.vertical, 12)
-                            .padding(.horizontal, 25)
-                            .background(Capsule().stroke(Color.white, lineWidth: 2))
-                    }
-                    .offset(x: -20, y: -50)
-                }
-                
-               
-            }
-        }
-        .frame(height: 200)
-        .background(
-            Color.white.opacity(0.2)
-                .cornerRadius(25)
-            
-                .rotation3DEffect(.init(degrees: 10), axis : (x:0, y: -1, z: 0))
-            // trim view
-                .padding(.vertical, 50)
-                .padding(.trailing, 25)
-            )
-        .padding(.horizontal)
-        .padding(.bottom, -90)
-    }
-}
-struct Snake : View {
-    @StateObject var appState = AppState.shared
-    var body: some View {
-        
-        HStack {
-            HStack{
-                Image("snake")
-                    .resizable()
-                    .frame(width: UIScreen.main.bounds.width / 3)
-                    .frame(width: 180, height: 160)
-                    .rotation3DEffect(.init(degrees: 180), axis : (x:0, y: -1, z: 0))
-                Spacer()
-            } .padding(.leading, -50)
-            
-            Text("Snake")
-                .multilineTextAlignment(.center)
-                .font(.system(size: 25, design: .monospaced))
-                .frame(maxWidth: 200)
-                .padding(.horizontal)
-                .shadow(color: Color.black.opacity(0.9), radius: 8, x: 2, y: 6)
-                .padding(.leading, -26)
-            VStack(spacing: 20){
-                Spacer(minLength: 0)
-                HStack{
-                    NavigationLink(destination: snake().id(appState.gameID)) {
-                        Text("Play")
-                            .font(.caption)
-                            .foregroundColor(.white)
-                            .padding(.vertical, 12)
-                            .padding(.horizontal, 25)
-                            .background(Capsule().stroke(Color.white, lineWidth: 2))
-                    }
-                    .offset(x: -20, y: -50)
-                }
-                
-               
-            }
-        }
-        .frame(height: 200)
-        .background(
-            Color.white.opacity(0.2)
-                .cornerRadius(25)
-            
-                .rotation3DEffect(.init(degrees: 10), axis : (x:0, y: -1, z: 0))
-            // trim view
-                .padding(.vertical, 50)
-                .padding(.trailing, 25)
-            )
-        .padding(.horizontal)
-        .padding(.bottom, -90)
-    }
-}
-struct UniverseMemory : View {
-//    var gamesData : HomeData
-//    let image : Image
-    var body: some View {
-        HStack {
-            HStack{
-                Image("universe")
-                    .resizable()
-                    .frame(width: UIScreen.main.bounds.width / 3)
-                    .frame(width: 50, height: 80)
-                Spacer()
-            } .padding(.leading, 20)
-            
-            Text("Universe Memory")
-                .multilineTextAlignment(.center)
-                .font(.system(size: 25, design: .monospaced))
-                .frame(maxWidth: 200)
-                .padding(.horizontal)
-                .shadow(color: Color.black.opacity(0.9), radius: 8, x: 2, y: 6)
-                .padding(.leading, -26)
-            VStack(spacing: 20){
-                Spacer(minLength: 0)
-                HStack{
-                    NavigationLink(destination:  MemoryGameView()) {
-                        Text("Play")
-                            .font(.caption)
-                            .foregroundColor(.white)
-                            .padding(.vertical, 12)
-                            .padding(.horizontal, 25)
-                            .background(Capsule().stroke(Color.white, lineWidth: 2))
-                    }
-                    .offset(x: -20, y: -50)
-                }
-                
-               
-            }
-        }
-        .frame(height: 200)
-        .background(
-            Color.white.opacity(0.2)
-                .cornerRadius(25)
-            
-                .rotation3DEffect(.init(degrees: 10), axis : (x:0, y: -1, z: 0))
-            // trim view
-                .padding(.vertical, 50)
-                .padding(.trailing, 25)
-            )
-        .padding(.horizontal)
-        .padding(.bottom, -90)
-    }
-}
-struct HomeData : Identifiable{
-    let id : Int
-    let image : String
-    let name : String
-    let description : String
-}
 
 struct ButtonTabBar: View {
     let image : Image
@@ -348,8 +139,3 @@ struct ButtonTabBar: View {
     }
 }
 
-
-var games = [HomeData(id: 0, image: "XO", name: "XO", description: ""),
-            HomeData(id: 1, image: "snake", name: "Snake", description: ""),
-            HomeData(id: 3, image: "puzzle", name: "Puzzle", description: ""),
-            HomeData(id: 4, image: "", name: "Universe Memory", description: "")]
